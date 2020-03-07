bdea6337af9fea2591ca156968346a4742f45f1b
Zbigniew Siciarz <zbigniew@siciarz.net>
2020-03-07 12:14:27 +0100

    WIP integration tests.

--- tests/test_nameko_prometheus.py
+++ tests/test_nameko_prometheus.py
@@ -1,5 +1,18 @@
-from nameko_prometheus import PrometheusMetrics  # noqa
+from nameko.web.handlers import http
+
+from nameko_prometheus import PrometheusMetrics
 
 
-def test_dependency_provider():
-    pass
+class MyService:
+    name = "my_service"
+    metrics = PrometheusMetrics(prefix="my_service")
+
+    @http("GET", "/metrics")
+    def serve_metrics(self, request):
+        return self.metrics.expose_metrics(request)
+
+
+def test_dependency_provider(rabbit_config, container_factory):
+    container = container_factory(MyService, rabbit_config)
+    container.start()
+    # TODO: collect some metrics, hit /metrics and check that they are exposed
