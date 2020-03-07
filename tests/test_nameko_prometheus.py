ec69a55635d6239b9407b20c73438b0b81ea4c35
Zbigniew Siciarz <zbigniew@siciarz.net>
2020-03-07 21:48:10 +0100

    Test what happens when HTTP endpoint raises an exception.

--- tests/test_nameko_prometheus.py
+++ tests/test_nameko_prometheus.py
@@ -2,6 +2,7 @@ import pytest
 from nameko.rpc import rpc
 from nameko.testing.services import entrypoint_hook
 from nameko.web.handlers import http
+from prometheus_client import REGISTRY
 
 from nameko_prometheus import PrometheusMetrics
 
@@ -12,6 +13,13 @@ def config(rabbit_config, web_config):
     config = rabbit_config.copy()
     config.update(web_config)
     return config
+
+
+@pytest.fixture(autouse=True)
+def reset_prometheus_registry():
+    collectors = list(REGISTRY._collector_to_names.keys())
+    for collector in collectors:
+        REGISTRY.unregister(collector)
 
 
 class MyService:
@@ -25,6 +33,10 @@ class MyService:
     @http("GET", "/metrics")
     def expose_metrics(self, request):
         return self.metrics.expose_metrics(request)
+
+    @http("GET", "/error")
+    def raise_error(self, request):
+        raise ValueError("poof")
 
 
 def test_expose_default_metrics(config, container_factory, web_session):
@@ -42,6 +54,16 @@ def test_expose_default_metrics(config, container_factory, web_session):
     )
 
 
+def test_http_metrics_collected_on_exception(config, container_factory, web_session):
+    container = container_factory(MyService, config)
+    container.start()
+    web_session.get("/error")
+    response = web_session.get("/metrics")
+    assert (
+        f'{MyService.name}_http_requests_total{{endpoint="/error",http_method="GET",status_code="500"}} 1.0'
+        in response.text
+    )
+
+
 # TODO:
 # - check custom-defined metrics (histograms etc)
-# - check HTTP status codes (include 500 when endpoint raises exception)
