8535af7de59be4bc8c2337468bddf72b0562ff39
Zbigniew Siciarz <zbigniew@siciarz.net>
2020-03-28 17:42:37 +0100

    Update current version to 0.1.1 in places other than setup.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = "nameko-prometheus"
 year = "2019"
 author = "Emplocity"
 copyright = "{0}, {1}".format(year, author)
-version = release = "0.1"
+version = release = "0.1.1"
 
 pygments_style = "trac"
 templates_path = ["."]
