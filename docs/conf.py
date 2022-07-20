4d9792e637e4f376e78b1a586ebe8026490a82fe
Zbigniew Siciarz <zbigniew@siciarz.net>
2022-07-20 10:40:46 +0200

    Bump version: 1.3.0 → 1.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = "nameko-prometheus"
 year = "2019"
 author = "Emplocity"
 copyright = "{0}, {1}".format(year, author)
-version = release = "1.3.0"
+version = release = "1.4.0"
 
 pygments_style = "trac"
 templates_path = ["."]
--- src/nameko_prometheus/__init__.py
+++ src/nameko_prometheus/__init__.py
@@ -1,5 +1,5 @@
 from nameko_prometheus.dependencies import PrometheusMetrics
 
-__version__ = "1.3.0"
+__version__ = "1.4.0"
 
 __all__ = ["PrometheusMetrics"]
